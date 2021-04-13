441746b3fcc35d84e7f6073a9908e894b6300f31
Rahimov Farhod <farhod.rahimov0055@gmail.com>
2021-04-13 15:27:26 +0300

    folder operations is normed

--- push_swap/operations/ft_push.c
+++ push_swap/operations/ft_push.c
@@ -1,33 +1,31 @@
 #include "../push_swap.h"
 
-void    ft_pa(t_stack **head_stack_a, t_stack **head_stack_b)
+void	ft_pa(t_stack **head_stack_a, t_stack **head_stack_b)
 {
-    t_stack *tmp1;
-    t_stack *tmp2;
+	t_stack	*tmp1;
+	t_stack	*tmp2;
 
-    write(1, "pa\n", 3);
-    if ((*head_stack_b) == NULL)
-        return ;
-    tmp1 = (*head_stack_b);
-    (*head_stack_b) = (*head_stack_b)->next;
-
-    tmp2 = (*head_stack_a);
-    tmp1->next = tmp2;
-    (*head_stack_a) = tmp1;
+	write(1, "pa\n", 3);
+	if ((*head_stack_b) == NULL)
+		return ;
+	tmp1 = (*head_stack_b);
+	(*head_stack_b) = (*head_stack_b)->next;
+	tmp2 = (*head_stack_a);
+	tmp1->next = tmp2;
+	(*head_stack_a) = tmp1;
 }
 
-void    ft_pb(t_stack **head_stack_a, t_stack **head_stack_b)
+void	ft_pb(t_stack **head_stack_a, t_stack **head_stack_b)
 {
-    t_stack *tmp1;
-    t_stack *tmp2;
+	t_stack	*tmp1;
+	t_stack	*tmp2;
 
-    write(1, "pb\n", 3);
-    if ((*head_stack_a) == NULL)
-        return ;
-    tmp1 = (*head_stack_a);
-    (*head_stack_a) = (*head_stack_a)->next;
-
-    tmp2 = (*head_stack_b);
-    tmp1->next = tmp2;
-    (*head_stack_b) = tmp1;
+	write(1, "pb\n", 3);
+	if ((*head_stack_a) == NULL)
+		return ;
+	tmp1 = (*head_stack_a);
+	(*head_stack_a) = (*head_stack_a)->next;
+	tmp2 = (*head_stack_b);
+	tmp1->next = tmp2;
+	(*head_stack_b) = tmp1;
 }
--- push_swap/operations/ft_rotate_reverse.c
+++ push_swap/operations/ft_rotate_reverse.c
@@ -1,11 +1,11 @@
 #include "../push_swap.h"
 
-void    ft_rra(t_stack **head_stack_a)
+void	ft_rra(t_stack **head_stack_a)
 {
-	t_stack *tmp;
-	t_stack *new_last;
+	t_stack	*tmp;
+	t_stack	*new_last;
 
-    write(1, "rra\n", 4);
+	write(1, "rra\n", 4);
 	if ((*head_stack_a) == NULL || ((*head_stack_a) && !(*head_stack_a)->next))
 		return ;
 	new_last = (*head_stack_a);
@@ -16,15 +16,16 @@ void    ft_rra(t_stack **head_stack_a)
 	tmp->next = (*head_stack_a);
 	(*head_stack_a) = tmp;
 }
-void    ft_rrb(t_stack **head_stack_b)
-{
-	t_stack *tmp;
-	t_stack *new_last;
 
-    write(1, "rrb\n", 4);
+void	ft_rrb(t_stack **head_stack_b)
+{
+	t_stack	*tmp;
+	t_stack	*new_last;
+
+	write(1, "rrb\n", 4);
 	if ((*head_stack_b) == NULL || ((*head_stack_b) && !(*head_stack_b)->next))
-        return ;
-    new_last = (*head_stack_b);
+		return ;
+	new_last = (*head_stack_b);
 	while (new_last->next->next)
 		new_last = new_last->next;
 	tmp = new_last->next;
@@ -32,9 +33,10 @@ void    ft_rrb(t_stack **head_stack_b)
 	tmp->next = (*head_stack_b);
 	(*head_stack_b) = tmp;
 }
-void    ft_rrr(t_stack **head_stack_a, t_stack **head_stack_b)
+
+void	ft_rrr(t_stack **head_stack_a, t_stack **head_stack_b)
 {
-    write(1, "rrr\n", 4);
+	write(1, "rrr\n", 4);
 	ft_rra(head_stack_a);
 	ft_rrb(head_stack_b);
 }
