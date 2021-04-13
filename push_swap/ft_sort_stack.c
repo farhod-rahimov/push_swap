1398f1248651b6d0031412c78f2d8910f3e0d2c3
Rahimov Farhod <farhod.rahimov0055@gmail.com>
2021-04-13 16:18:49 +0300

    still working on that

--- push_swap/ft_sort_stack.c
+++ push_swap/ft_sort_stack.c
@@ -13,80 +13,81 @@ void	ft_sort_stack_main(t_stack **head_stack_a, t_stack *head_stack_b, int list_
 	free(array);
 }
 
-// void	ft_sort_stack_a_part1(t_stack **head_stack_a, t_stack **head_stack_b, int list_size, int sorted_a)
-// {
-// 	int	median;
-// 	int	i;
-// 	int	b;
+int	ft_sort_stack_a_part1(t_sort *strct, int k)
+{
+	int	median;
+	int	tmp_segm_size;
+	int	i;
+	int	b;
 
-// 	while (list_size > 3)
-// 	{
-// 		median = ft_get_median((*head_stack_a), list_size);
-// 		i = 0;
-// 		b = 0;
-// 		tmp_segm_size = ft_lstsize((t_list *)(*head_stack_b));
-// 		while (i < list_size)
-// 		{
-// 			if ((*head_stack_a)->value > median)
-// 				ft_pb(head_stack_a, head_stack_b);
-// 			else
-// 			{
-// 				ft_ra(head_stack_a);
-// 				b++;
-// 			}
-// 			i++;
-// 		}
-// 		while (b-- > 0 && sorted_a > 0)
-// 			ft_rra(head_stack_a);
-// 		strct.segment_size[k++] = ft_lstsize((t_list *)(*head_stack_b)) - tmp_segm_size;
-// 		list_size = ft_lstsize((t_list *)(*head_stack_a)) - sorted_a;
-// 	}
-// }
+	while (strct->list_size > 3)
+	{
+		median = ft_get_median((*strct->head_stack_a), strct->list_size);
+		i = 0;
+		b = 0;
+		tmp_segm_size = ft_lstsize((t_list *)(*strct->head_stack_b));
+		while (i < strct->list_size)
+		{
+			if ((*strct->head_stack_a)->value > median)
+				ft_pb(strct->head_stack_a, strct->head_stack_b);
+			else
+			{
+				ft_ra(strct->head_stack_a);
+				b++;
+			}
+			i++;
+		}
+		while (b-- > 0 && strct->sorted_a > 0)
+			ft_rra(strct->head_stack_a);
+		strct->segment_size[k++] = ft_lstsize((t_list *)(*strct->head_stack_b)) - tmp_segm_size;
+		strct->list_size = ft_lstsize((t_list *)(*strct->head_stack_a)) - strct->sorted_a;
+	}
+	return (k);
+}
 
 int	ft_sort_stack_a(t_stack **head_stack_a, t_stack **head_stack_b, int list_size, int sorted_a)
 {
 	t_sort strct;
 	int	*array;
-	// int	*segment_size;
-	int	tmp_segm_size;
-	int	median;
+	// int	tmp_segm_size;
+	// int	median;
 	int	i;
-	int	b;
+	// int	b;
 	int	k;
 
-	// segment_size = (int *)malloc(sizeof(int) * (list_size / 2));
 	strct.head_stack_a = head_stack_a;
 	strct.head_stack_b = head_stack_b;
 	strct.list_size = list_size;
 	strct.sorted_a = sorted_a;
-	strct.segment_size = ft_create_blank_array(list_size / 2);
-	ft_bzero(strct.segment_size, (list_size / 2) * sizeof(int));
-	k = 0;
-	while (list_size > 3)
-	{
-		median = ft_get_median((*strct.head_stack_a), list_size);
-		i = 0;
-		b = 0;
-		tmp_segm_size = ft_lstsize((t_list *)(*strct.head_stack_b));
-		while (i < list_size)
-		{
-			if ((*strct.head_stack_a)->value > median)
-				ft_pb(strct.head_stack_a, strct.head_stack_b);
-			else
-			{
-				ft_ra(strct.head_stack_a);
-				b++;
-			}
-			i++;
-		}
-		while (b-- > 0 && strct.sorted_a > 0)
-			ft_rra(strct.head_stack_a);
-		strct.segment_size[k++] = ft_lstsize((t_list *)(*strct.head_stack_b)) - tmp_segm_size;
-		list_size = ft_lstsize((t_list *)(*strct.head_stack_a)) - strct.sorted_a;
-	}
-	if (list_size == 2)
+	strct.segment_size = ft_create_blank_array(strct.list_size / 2);
+	ft_bzero(strct.segment_size, (strct.list_size / 2) * sizeof(int));
+	// k = 0;
+	k = ft_sort_stack_a_part1(&strct, 0);
+	// while (strct.list_size > 3)
+	// {
+	// 	median = ft_get_median((*strct.head_stack_a), strct.list_size);
+	// 	i = 0;
+	// 	b = 0;
+	// 	tmp_segm_size = ft_lstsize((t_list *)(*strct.head_stack_b));
+	// 	while (i < strct.list_size)
+	// 	{
+	// 		if ((*strct.head_stack_a)->value > median)
+	// 			ft_pb(strct.head_stack_a, strct.head_stack_b);
+	// 		else
+	// 		{
+	// 			ft_ra(strct.head_stack_a);
+	// 			b++;
+	// 		}
+	// 		i++;
+	// 	}
+	// 	while (b-- > 0 && strct.sorted_a > 0)
+	// 		ft_rra(strct.head_stack_a);
+	// 	strct.segment_size[k++] = ft_lstsize((t_list *)(*strct.head_stack_b)) - tmp_segm_size;
+	// 	strct.list_size = ft_lstsize((t_list *)(*strct.head_stack_a)) - strct.sorted_a;
+	// }
+	if (strct.list_size == 2)
 		strct.sorted_a = ft_sort_if_2_elements(strct.head_stack_a, strct.sorted_a);
-	else if (list_size == 3)
+	else if (strct.list_size == 3)
 		strct.sorted_a = ft_sort_if_3_elements(strct.head_stack_a, strct.sorted_a);
 	while (--k >= 0)
 	{
