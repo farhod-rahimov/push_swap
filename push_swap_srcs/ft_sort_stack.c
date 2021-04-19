bfa227d653b5e8d15c5bf38abc00246c7fb6e351
Rahimov Farhod <farhod.rahimov0055@gmail.com>
2021-04-19 18:45:28 +0300

    вроде все работает, протестировать и подогнать под норму и НЕ забудь ft_5_el

--- push_swap_srcs/ft_sort_stack.c
+++ push_swap_srcs/ft_sort_stack.c
@@ -8,6 +8,7 @@ void	ft_sort_stack_main(t_stack **head_stack_a, \
 	array = ft_create_array_from_list(list_size, (*head_stack_a));
 	if (!ft_check_if_stack_is_sorted(array, list_size))
 		ft_sort_stack_a(head_stack_a, &head_stack_b, list_size, sorted_a);
+	// printf("SB\n"); ft_print(head_stack_b);
 	free(array);
 }
 
@@ -43,23 +44,37 @@ int	ft_sort_stack_a_part1(t_sort *s, int k, int i, int b)
 void	ft_sort_stack_a_part2(t_sort *s, int k, int i)
 {
 	int	*array;
+	int b;
+	int tmp_b;
+	int median;
 
 	if ((*s->head_stack_b) && ft_lstsize((t_list *)(*s->head_stack_b)) < 3)
 		ft_sort_stack_a(s->head_stack_b, NULL, \
 					ft_lstsize((t_list *)(*s->head_stack_b)), 0);
+	median = ft_get_median(*s->head_stack_b, s->segm_size[k]);
+	b = 0;
 	while (i < s->segm_size[k])
 	{
-		ft_pa(s->head_stack_a, s->head_stack_b);
+		if ((*s->head_stack_b)->value <= median)
+			ft_pa(s->head_stack_a, s->head_stack_b);
+		else
+		{
+			ft_rb(s->head_stack_b);
+			b++;
+		}
 		i++;
 	}
-	array = ft_create_array_from_list(s->segm_size[k], (*s->head_stack_a));
-	if (!ft_check_if_stack_is_sorted(array, s->segm_size[k]))
+	tmp_b = b;
+	while (tmp_b-- > 0)
+		ft_rrb(s->head_stack_b);
+	array = ft_create_array_from_list(s->segm_size[k] - b, (*s->head_stack_a));
+	if (!ft_check_if_stack_is_sorted(array, s->segm_size[k] - b))
 		s->sorted_a = ft_sort_stack_a(s->head_stack_a, \
-		s->head_stack_b, s->segm_size[k], s->sorted_a);
+		s->head_stack_b, s->segm_size[k] - b, s->sorted_a);
 	else
 	{
 		i = 0;
-		while (i < s->segm_size[k])
+		while (i < s->segm_size[k] - b)
 		{
 			ft_ra(s->head_stack_a);
 			i++;
@@ -67,6 +82,9 @@ void	ft_sort_stack_a_part2(t_sort *s, int k, int i)
 		s->sorted_a += i;
 	}
 	free(array);
+	s->segm_size[k] = b;
+	if (b)
+		ft_sort_stack_a_part2(s, k, 0);
 }
 
 int	ft_sort_stack_a(t_stack **head_stack_a, \
@@ -81,16 +99,21 @@ int	ft_sort_stack_a(t_stack **head_stack_a, \
 	s.sorted_a = sorted_a;
 	s.segm_size = ft_create_blank_array(s.list_size / 2);
 	ft_bzero(s.segm_size, (s.list_size / 2) * sizeof(int));
+	// printf("A "); ft_print(*head_stack_a);
 	k = ft_sort_stack_a_part1(&s, 0, 0, 0);
+	// printf("B "); ft_print(*head_stack_a);
 	if (s.list_size == 2)
 		s.sorted_a = ft_sort_if_2_elements(s.head_stack_a, \
 										s.sorted_a);
 	else if (s.list_size == 3)
 		s.sorted_a = ft_sort_if_3_elements(s.head_stack_a, \
 										s.sorted_a);
+	// printf("C "); ft_print(*head_stack_a);
 	while (--k >= 0)
 	{
+		// printf("D "); ft_print(*head_stack_a);
 		ft_sort_stack_a_part2(&s, k, 0);
+		// printf("E "); ft_print(*head_stack_a);
 	}
 	free(s.segm_size);
 	return (s.sorted_a);
--- push_swap_srcs/main.c
+++ push_swap_srcs/main.c
@@ -19,5 +19,13 @@ int	main(int argc, char **argv)
 	ft_get_stack_data(head_stack_a, argv, i);
 	ft_sort_stack_main(&head_stack_a, NULL, \
 		ft_lstsize((t_list *)head_stack_a), 0);
+	
+	// printf("SA\n"); ft_print(head_stack_a);
+	// int *array;
+	// array = ft_create_array_from_list(ft_lstsize((t_list *)head_stack_a), head_stack_a);
+	// if (ft_check_if_stack_is_sorted(array, ft_lstsize((t_list *)head_stack_a)))
+	// 	printf("SORTTED\n");
+	// else
+	// 	printf("NOT\n");
 	return (0);
 }
