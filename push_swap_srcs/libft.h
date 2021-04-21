b042f63f3e94bd4b52bbc41f0c5a115bdb2a9ebc
Rahimov Farhod <farhod.rahimov0055@gmail.com>
2021-04-21 15:01:26 +0300

    still working on that

--- push_swap_srcs/libft.h
+++ push_swap_srcs/libft.h
@@ -25,6 +25,7 @@ char				*ft_strrchr(const char *s, int c);
 char				*ft_strnstr(const char *haystack, const char *needle, \
 					size_t len);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
+int					ft_strcmp(char *s1, char *s2);
 long long int		ft_atoi(const char *str);
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
--- push_swap_srcs/utils/ft_strcmp.c
+++ push_swap_srcs/utils/ft_strcmp.c
@@ -1,4 +1,4 @@
-#include "../checker.h"
+#include "../push_swap.h"
 
 int	ft_strcmp(char *s1, char *s2)
 {
